8a0ebd4552e81458bc75fd4515a53b993e5d9534
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-11-23 21:42:05 +0700

    Membuat navigasi ke edit profil, change password, dan logout

--- app/src/main/java/com/azhara/perintismitraapp/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/azhara/perintismitraapp/home/profile/ProfileFragment.kt
@@ -1,34 +1,18 @@
 package com.azhara.perintismitraapp.home.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.azhara.perintismitraapp.R
+import com.azhara.perintismitraapp.auth.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class ProfileFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +22,25 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_edit_profile.setOnClickListener(this)
+        btn_change_password.setOnClickListener(this)
+        btn_logout.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_edit_profile -> view?.findNavController()?.navigate(R.id.action_profileFragment_to_editProfileFragment)
+            R.id.btn_change_password -> view?.findNavController()?.navigate(R.id.action_profileFragment_to_changePasswordFragment)
+            R.id.btn_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(context, MainActivity::class.java))
+                activity?.finish()
+            }
+        }
+    }
+
+
 }
